1fe13a240adc50e3de9a47f097ada2baec69c033
kasi <kasionaga@gmail.com>
2015-10-03 18:18:21 -1000

    arlkwjer

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,33 @@
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 
-var PathTo = {
-  SassFiles: './sass/**/*.scss',
-  PublicFolder: './public',
-  PublicCss: './public/styles',
-  PublicCssFiles: './public/styles/*.css'
-};
-
-gulp.task('watch-files', function (){
-  gulp.watch(PathTo.SassFiles, ['compile-sass']);
-  gulp.watch(PathTo.PublicCssFiles, ['html']);
-});
-
-gulp.task('compile-sass', function (){
-  return gulp
-          .src(PathTo.SassFiles, ['compile-sass'])
-          .pipe(sass({ errLogToConsole: true }))
-          .pipe(gulp.dest(PathTo.PublicCss));
-});
-
-gulp.task('html', function (){
-  return gulp.src('./public/index.html')
-    .pipe(connect.reload());
-});
-
-gulp.task('public-server', function (){
+gulp.task('connect', function(){
   connect.server({
-    root: './public',
-    port: 8080,
+    root: 'public',
     livereload: true
   });
 });
 
-gulp.task('default', ['public-server', 'compile-sass', 'watch-files']);
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+      .pipe(sass({
+        errLogToConsole: true,
+        sourceComments: true,
+        includePaths: ['bower_components/foundation/scss']
+      }).on('error', sass.logError))
+      .pipe(gulp.dest('./public/styles'));
+});
+
+gulp.task('livereload', function (){
+  gulp.src('./public/**/*')
+  .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./public/**/*', ['livereload']);
+});
+
+gulp.task('default', ['connect', 'watch', 'sass']);
